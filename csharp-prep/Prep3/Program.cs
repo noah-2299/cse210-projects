2a51dff53fed3dc0edbf9469580783c22b8268f2
noah-2299 <noahclarksmith@gmail.com>
2023-01-10 20:30:52 -0500

    Prep assignments 3, 4, and 5 Core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //
+        string keepPlaying = "yes";
+        while (keepPlaying == "yes")
+        {
+        //Create a new Random Magic number for the user to guess
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+        // Set the variable of guess to an integer out side of the range so that the data type is set but it cannot match before the user has guessed
+        int guess = 0;
+        // do while runs the first time and gets a guess, loop keeps running until the user guesses right.
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed It!");
+            }
+        } while (guess!= magicNumber);
+        Console.WriteLine("Would you like to play again? ");
+        keepPlaying = Console.ReadLine();
+        }
     }
 }
