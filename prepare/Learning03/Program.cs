32b0a1d18e49883bcf93c9035d81a9e3671c5e0c
noah-2299 <noahclarksmith@gmail.com>
2023-02-01 20:16:25 -0500

    Practise with class and encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        int top = 6;
+        int bottom = 7;
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(top);
+        Fraction f3 = new Fraction(top,bottom);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f3.GetFractionString());
     }
 }
